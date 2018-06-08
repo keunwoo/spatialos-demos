357275e5cb1bf04134be6151e3b38dc26745d844
Keunwoo Lee <k@keunwoo.com>
2018-06-08 16:24:20 -0700

    add basic, dumb SpawnerReaper worker
    This worker simply spawns "ticktock" entities in a 5x5 grid near the
    origin, and then deletes them after about 10 seconds.  Hopefully this
    code gives at least a little flavor of what it's like to program
    against "bare metal" SpatialOS C++ APIs, without the Unity layers that
    most of the public SpatialOS tutorials assume.
    
    There are many deficiencies in the design and implementation of this
    worker (read the comments for some of them), but I am pushing this up
    just to share what I've done so far.

--- workers/SpawnerReaper/src/startup.cc
+++ workers/SpawnerReaper/src/startup.cc
@@ -1,8 +1,17 @@
 #include <improbable/worker.h>
 #include <improbable/standard_library.h>
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
 #include <algorithm>
 #include <ctime>
+
+#include <chrono>
+#include <mutex>
+#include <queue>
+#include <sstream>
+#include <thread>
+#include <unordered_set>
 
 // Use this to make a worker::ComponentRegistry.
 // For example use worker::Components<improbable::Position, improbable::Metadata> to track these common components
@@ -35,14 +44,21 @@ std::string get_random_characters(size_t count) {
     return str;
 }
 
+template <typename Rep, typename Period>
+std::string duration_str(const std::chrono::duration<Rep, Period> &d) {
+    std::stringstream s;
+    s << d.count() << "(" << Period::num << "/" << Period::den << ")s";
+    return s.str();
+}
+
 // Entry point
 int main(int argc, char** argv) {
     srand(time(nullptr));
 
-    std::cout << "[local] Worker started " << std::endl;
+    std::cerr << "[local] Worker started " << std::endl;
 
     auto print_usage = [&]() {
-        std::cout << "Usage: SpawnerReaper receptionist <hostname> <port> <worker_id>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " receptionist <hostname> <port> <worker_id>" << std::endl;
         std::cout << std::endl;
         std::cout << "Connects to SpatialOS" << std::endl;
         std::cout << "    <hostname>      - hostname of the receptionist or locator to connect to.";
@@ -82,7 +98,7 @@ int main(int argc, char** argv) {
         workerId = parameters.WorkerType + "_" + get_random_characters(4);
     }
 
-    std::cout << "[local] Connecting to SpatialOS as " << workerId << "..." << std::endl;
+    std::cerr << "[local] Connecting to SpatialOS as " << workerId << "..." << std::endl;
 
     // Connect with receptionist
     worker::Connection connection = ConnectWithReceptionist(arguments[1], atoi(arguments[2].c_str()), workerId, parameters);
@@ -93,27 +109,205 @@ int main(int argc, char** argv) {
     worker::Dispatcher dispatcher{ ComponentRegistry{} };
     bool is_connected = connection.IsConnected();
 
-    dispatcher.OnDisconnect([&](const worker::DisconnectOp& op) {
+    dispatcher.OnDisconnect([&is_connected](const worker::DisconnectOp& op) {
         std::cerr << "[disconnect] " << op.Reason << std::endl;
         is_connected = false;
     });
 
     // Print log messages received from SpatialOS
-    dispatcher.OnLogMessage([&](const worker::LogMessageOp& op) {
+    dispatcher.OnLogMessage([](const worker::LogMessageOp& op) {
         if (op.Level == worker::LogLevel::kFatal) {
             std::cerr << "Fatal error: " << op.Message << std::endl;
             std::terminate();
         }
-        std::cout << "[remote] " << op.Message << std::endl;
+        std::cerr << "[remote] " << op.Message << std::endl;
     });
 
+    // Track creation requests and created entities.
+    std::unordered_set<worker::RequestId<worker::CreateEntityRequest>> create_requests;
+    std::mutex create_requests_mutex;  // guards create_requests
+
+    // Add an event handler for OnCreateEntityResponse.
+    //
+    // We put created entities in a priority queue, with earlier entities
+    // earlier.  We will later use this to reap (delete) entities that we
+    // created.
+    //
+    // This is a quick & dirty hack, not a good worker design, because an
+    // entity's age lives only in the memory space of the worker process that
+    // created it.  Workers may be freely killed and restarted at any time by
+    // SpatialOS, so if the worker that created an entity were to die, then the
+    // creation age would be forgotten, and no replacement worker would ever be
+    // able to reap it.
+    //
+    // A better design would treat the worker's memory as ephemeral.  Since we
+    // want an entity's creation time to determine its fate even across worker
+    // restarts, this state should be saved in a component attached to the
+    // entity.  Then, each worker should inspect the entities in its authority,
+    // and reap old entities.  We might implement this in a future revision of
+    // this demo.
+    using timestamp = std::chrono::time_point<std::chrono::steady_clock>;
+    using timestamped_entity = std::pair<worker::EntityId, timestamp>;
+    auto priority_greater = [](const timestamped_entity &a, const timestamped_entity &b) {
+        return a.first > b.first;
+    };
+    std::priority_queue<
+        timestamped_entity,
+        std::vector<timestamped_entity>,
+        decltype(priority_greater)
+    > created(priority_greater);
+    std::mutex created_mutex;
+    dispatcher.OnCreateEntityResponse(
+        [
+            &create_requests,
+            &create_requests_mutex,
+            &created,
+            &created_mutex
+        ]
+        (const worker::CreateEntityResponseOp& op) {
+            auto ts = std::chrono::steady_clock::now();
+
+            bool ignore = false;
+            {
+                std::lock_guard<std::mutex> lock(create_requests_mutex);
+                auto req = create_requests.find(op.RequestId);
+                ignore = req == create_requests.end();
+                if (!ignore) {
+                    create_requests.erase(req);
+                }
+            }
+
+            // We pull this stanza out of the critical section to avoid holding the lock longer.
+            if (ignore) {
+                std::cerr << "[local] ignoring create request that we did not trigger: " << op.RequestId.Id << std::endl;
+                return;
+            }
+            if (op.StatusCode != worker::StatusCode::kSuccess) {
+                std::cerr << "[local] failed creation request: " << op.RequestId.Id
+                          << "; reason: " << op.Message
+                          << std::endl;
+                return;
+            }
+
+            {
+                std::lock_guard<std::mutex> lock(create_requests_mutex);
+                created.emplace(*op.EntityId, ts);
+            }
+        }
+    );
+
+    // Connections are not thread-safe.  In the code above, we have relied on
+    // the fact that, during startup, the main thread is the only thread.
+    // However, we are about to define the spawning/reaping loop, which will run
+    // on its own thread, so we need a mutex now.
+    //
+    // This mutex guards access to the connection object after multiple threads
+    // are spun up.
+    //
+    // If you were writing a lot more code, you'd probably write a utility class
+    // that performs mutex acquisition/release while acting as a Connection
+    // handle, and make sure that all Connection access goes through this class.
+    std::mutex connection_mutex;
+
+    auto tick_fn = [
+        &connection,
+        &connection_mutex,
+        &create_requests,
+        &create_requests_mutex,
+        &created,
+        &created_mutex
+    ]() {
+        while (true) {
+            // This is a very dumb simulation loop that just does some stuff and
+            // then sleeps for a second.  A proper loop would calibrate the
+            // "frame rate"/sleep time based on the actual time elapsed in the
+            // body of the loop below.
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+            {
+                std::lock_guard<std::mutex> lock(connection_mutex);
+                // The docs are wrong: worker::LogLevel enum cases use
+                // Google-style "kFooBar" naming, not "FooBar".
+                connection.SendLogMessage(worker::LogLevel::kInfo, "timer", "tick (will try to create an entity)");
+            }
+
+            // Set up a dummy entity, positioned randomly in a 5x5 square.
+            worker::Entity ent;
+            improbable::Position::Data pos;
+            // Note that Y is "up" in SpatialOS, so X & Z translate
+            // horizontally, i.e. the spatial inspector's coordinate system
+            // looks like this:
+            //
+            //      Z
+            //      ^
+            //      |
+            //   <--+--> X
+            //      |
+            //      v
+            //
+            // with Y projecting "out of the screen".
+            pos.set_coords(improbable::Coordinates{double(rand() % 5), 0, double(rand() % 5)});
+            ent.Add<improbable::Position>(pos);
+            improbable::Metadata::Data meta;
+            meta.set_entity_type("ticktock");
+            ent.Add<improbable::Metadata>(meta);
+
+            // Send the creation request.
+            worker::RequestId<worker::CreateEntityRequest> create_req_id;
+            {
+                std::lock_guard<std::mutex> lock(connection_mutex);
+                create_req_id = connection.SendCreateEntityRequest(ent, {}, {});
+            }
+            {
+                std::lock_guard<std::mutex> lock(create_requests_mutex);
+                create_requests.insert(create_req_id);
+            }
+            std::cerr << "[local] sent entity creation request: " << create_req_id.Id << std::endl;
+
+            // Reap any entities <10s old
+            std::vector<worker::EntityId> to_reap;
+            const auto now = std::chrono::steady_clock::now();
+            {
+                std::lock_guard<std::mutex> lock(created_mutex);
+                while (!created.empty()) {
+                    const auto top = created.top();
+                    if (now - top.second < std::chrono::seconds(10)) {
+                        break;
+                    } else {
+                        created.pop();
+                        to_reap.push_back(top.first);
+                    }
+                }
+            }
+            while (!to_reap.empty()) {
+                auto back = to_reap.back();
+                to_reap.pop_back();
+                std::cerr << "[local] reaping: " << back << std::endl;
+                // TODO(keunwoo): Add logic that buffers outstanding deletion requests
+                // and retries them on failure.
+                {
+                    std::lock_guard<std::mutex> lock(connection_mutex);
+                    connection.SendDeleteEntityRequest(back, {});
+                }
+            }
+        }
+    };
+
     if (is_connected) {
-        std::cout << "[local] Connected successfully to SpatialOS, listening to ops... " << std::endl;
+        std::cerr << "[local] starting timer thread..." << std::endl;
+        std::thread timer(tick_fn);
+        timer.detach();
+
+        std::cerr << "[local] Connected successfully to SpatialOS, listening to ops... " << std::endl;
     }
 
     while (is_connected) {
-        dispatcher.Process(connection.GetOpList(kGetOpListTimeoutInMilliseconds));
+        worker::Option<worker::OpList> op_list;
+        {
+            std::lock_guard<std::mutex> lock(connection_mutex);
+            op_list.emplace(connection.GetOpList(kGetOpListTimeoutInMilliseconds));
+        }
+        dispatcher.Process(*op_list);
     }
-
-    return ErrorExitStatus;
+    exit(ErrorExitStatus);
 }
